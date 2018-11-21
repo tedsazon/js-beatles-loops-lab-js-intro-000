6fdc0a3c3568df286ab78e06aa20ec6e67eeb8ba
Ted Sazon <tedsazon@gmail.com>
2018-11-21 18:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-function theBeatlesPlay(muiscians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   const arr = []
-
-  for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`${muiscians[i]} plays ${muiscians[i]}`)
-  }
+  for (let i = 0, l = musicians.length; i < l; i++)
+    arr.push(`${musicians[i]} plays ${instruments[i]}`) 
+}
   return arr
 }
