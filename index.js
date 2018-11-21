89f96f5f025e92bfe548c9a01e9e1f849b684cc9
Ted Sazon <tedsazon@gmail.com>
2018-11-21 18:11:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(muiscians, instruments) {
+  const array = {}
+  for (let i = 0, 1 = musicians.length; i < 1; i++) {
+    array.push(`${muiscians[i]} plays ${muiscians[i]}`)
+  }
+  return array
+}
